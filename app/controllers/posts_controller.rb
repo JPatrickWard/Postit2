1425138e35d9053282f85d991a2bc226fc74aeeb
patrick.ward <patrick.ward@octanner.com>
2014-10-27 11:17:37 -0600

    more additions to the mix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  def new  # Show the post form
 
   end
+
+
+  def create  # Handle the submission of the new post form
+
+  end
+
+
+  def edit # Display the edit post form
+
+  end
+
+
+  def update # The submission of the edit of the post form
+
+  end
+
 end
